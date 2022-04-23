071bbf902f21a55e59bd12a62d4e441b3c6244f2
Grzegorz <grzes.kotarski@gmail.com>
2022-04-23 21:45:48 +0200

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,22 +1,25 @@
 """
 Unit tests for RPN Calculator.
 """
-import decimal
+from decimal import Decimal
 from pyrpnapi.calc import calculate
 
+def test_single_value():
+    "Test single value"
+    assert calculate('123') == Decimal(123)
 
 def test_add():
     "Test adding"
-    assert calculate('1 2 +'.split())==decimal.Decimal(3)
+    assert calculate('1 2 +'.split()) == Decimal(3)
 
 def test_sub():
     "Test substraction"
-    assert calculate('150 50 -'.split())==decimal.Decimal(150-50)
+    assert calculate('150 50 -'.split()) == Decimal(150-50)
 
 def test_mul():
     "Test multiplication"
-    assert calculate('4 50 * '.split())==decimal.Decimal(4*50)
+    assert calculate('4 50 * '.split()) == Decimal(4*50)
 
 def test_div():
     "Test division"
-    assert calculate('100 25 /'.split())==decimal.Decimal(100/25)
+    assert calculate('100 25 /'.split()) == Decimal(100/25)
