64283326846c58e5e76f7736a04d7470c47aa984
Grzegorz <grzes.kotarski@gmail.com>
2022-05-16 14:51:20 +0200

    Update rpn.py

--- pyrpnapi/rpn.py
+++ pyrpnapi/rpn.py
@@ -1,26 +1,17 @@
-"""
-RPN Calculator API
-"""
 
 from fastapi import FastAPI
-from pydantic import BaseModel  # pylint: disable=no-name-in-module
+from pydantic import BaseModel
 from pyrpnapi.calc import calculate
 
 
 app = FastAPI()
 
 
-class Expression(BaseModel):  # pylint: disable=too-few-public-methods
-    """
-    Expression model class.
-    """
+class Expression(BaseModel):
     expr: str
 
 
 @app.post("/eval/")
 async def evaluate(expression: Expression):
-    """
-    Evaluate the expression endpoint.
-    """
     value = calculate(expression.expr.split())
     return {"value": value}
